b6eac0c9312e59f452cec59d0886556c96c6df9c
Arsath99 <90707804+Arsath99@users.noreply.github.com>
2022-11-28 22:21:51 +0530

    Background removed

---
+++ Removebg.py
@@ -0,0 +1,10 @@
+from rembg import remove
+from PIL import Image
+
+input_path = 'input.jpeg'
+output_path = 'output.png'
+
+input = Image.open(input_path)
+output = remove(input)
+
+output.save(output_path)
